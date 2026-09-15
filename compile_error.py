96ad09906b7add947bab16d051183edcf4a680fa
corruptedk1d <d3ass13@ya.ru>
2026-09-15 15:40:49 +0300

    b2 1st commit fix conflict

--- compile_error.py
+++ compile_error.py
@@ -1,8 +1,2 @@
 out = (1).__class__.__base__.__subclasses__()
-<<<<<<< HEAD
-print('remote change 2')
-=======
-print('remote change')
-print('another local change')
->>>>>>> 81c56fd (another)
-print('Using b2!')
+print('I have cleaned all')
